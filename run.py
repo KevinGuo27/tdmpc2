6ab42ecac2e7128645ba2b442cd78d3ac43a74b7
KevinGuo27 <113325820+KevinGuo27@users.noreply.github.com>
2024-04-10 18:50:28 -0400

    create a branch kevin

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from agents import get_agent
 from envs import get_env
 from models import get_model
 # main function to run the code
+# on branch kevin
 if __name__ == '__main__':
     args = Hyperparams().parse_args()
     model = get_model()
